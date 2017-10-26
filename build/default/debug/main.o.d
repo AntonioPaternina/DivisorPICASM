ef3b83f118c9a7780aee6537587328a34834f501
Antonio Paternina <antoniojxk@gmail.com>
2017-10-26 12:18:50 -0500

    versión inicial, funciona captura de ambos operandos, división, falta corregir bugs en parpadeo de resultado

---
+++ build/default/debug/main.o.d
@@ -0,0 +1,3 @@
+build/default/debug/main.o:  \
+	C:/projects/poli/arquitectura-de-computadores/TallerDivisionASM.X/main.asm  \
+
---
+++ build/default/production/main.o.d
@@ -0,0 +1,3 @@
+build/default/production/main.o:  \
+	C:/projects/poli/arquitectura-de-computadores/TallerDivisionASM.X/main.asm  \
+
